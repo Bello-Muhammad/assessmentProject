c1795b4c2ac8bd1434bc75db3550d6cc2e0ea739
Bello Muhammad <67134404+Bello-Muhammad@users.noreply.github.com>
2023-09-21 23:01:01 +0100

    Update listener.ts

--- taskDomain/src/listener.ts
+++ taskDomain/src/listener.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.RMQ,
     options: {
-      urls: ['amqps://vcxaamlr:2qU-Gs5REuY8-YAZ3schBE5rB_yhIAhM@rat.rmq2.cloudamqp.com/vcxaamlr'],
+      urls: [process.env.RABBITMQ_URL],
       queue: 'main_queue',
       queueOptions: {
         durable: false
